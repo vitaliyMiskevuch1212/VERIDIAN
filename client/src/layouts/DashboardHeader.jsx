ccc87847b43ccce4e3ad7f0f6a1619074a58a896
vitaliyMiskevuch1212 <miskotop1234@gmail.com>
2025-02-19 11:49:37 +0200

    feat: integrate Navbar and Ticker with global data and UI context (index.css, DashboardHeader.jsx etc)

---
+++ client/src/layouts/DashboardHeader.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import Ticker from '../components/Ticker';
+import Navbar from '../components/Navbar';
+import { useData } from '../context/DataContext';
+import { useUI } from '../context/UIContext';
