7e80231eab2b7c7b88e1047f4fb441a65fb10297
vitaliyMiskevuch1212 <miskotop1234@gmail.com>
2025-02-19 12:24:20 +0200

    feat: implement DashboardHeader with system status and control hooks (index.css, DashboardHeader.jsx etc)

--- client/src/layouts/DashboardHeader.jsx
+++ client/src/layouts/DashboardHeader.jsx
@@ -3,3 +3,14 @@ import Ticker from '../components/Ticker';
 import Navbar from '../components/Navbar';
 import { useData } from '../context/DataContext';
 import { useUI } from '../context/UIContext';
+
+export default function DashboardHeader() {
+  const { news, isConnected, serverClients } = useData();
+  const { 
+    missionMetrics, 
+    isCommsActive, 
+    handleCommsToggle, 
+    setActiveTab, 
+    defconLevel,
+    handleTickerEventClick 
+  } = useUI();
