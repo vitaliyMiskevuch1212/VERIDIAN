2d060cb2cadb442fcf46d8e28f3eb7dd83563828
vitaliyMiskevuch1212 <miskotop1234@gmail.com>
2025-03-09 11:15:53 +0200

    Merge branch 'main' into Server_routes

--- client/src/layouts/DashboardLeftPanel.jsx
+++ client/src/layouts/DashboardLeftPanel.jsx
@@ -3,12 +3,3 @@ import NewsPanel from '../components/NewsPanel';
 import ErrorBoundary from '../components/ErrorBoundary';
 import { useData } from '../context/DataContext';
 import { useUI } from '../context/UIContext';
-
-export default function DashboardLeftPanel() {
-  const { news, newsLoading } = useData();
-  const { 
-    panelsVisible, 
-    activeFilters, 
-    timeRange, 
-    setWargameEvent 
-  } = useUI();
