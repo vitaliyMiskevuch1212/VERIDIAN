2f6275417dbbdfeb5c76db8fa3a90884463374ae
vitaliyMiskevuch1212 <miskotop1234@gmail.com>
2025-03-09 14:07:50 +0200

    Merge branch 'main' into frontend

--- client/src/layouts/DashboardLeftPanel.jsx
+++ client/src/layouts/DashboardLeftPanel.jsx
@@ -3,3 +3,13 @@ import NewsPanel from '../components/NewsPanel';
 import ErrorBoundary from '../components/ErrorBoundary';
 import { useData } from '../context/DataContext';
 import { useUI } from '../context/UIContext';
+
+export default function DashboardLeftPanel() {
+  const { news, newsLoading } = useData();
+  const { 
+    panelsVisible, 
+    activeFilters, 
+    timeRange, 
+    setWargameEvent 
+  } = useUI();
+import { useUI } from '../context/UIContext';
