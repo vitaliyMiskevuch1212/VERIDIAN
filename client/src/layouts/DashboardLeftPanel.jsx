a4031e0f62cf3597bfc9b78c29dace565b119d23
vitaliyMiskevuch1212 <miskotop1234@gmail.com>
2025-03-09 18:17:54 +0200

    Merge pull request #63 from Tiwari1782/Server_main

--- client/src/layouts/DashboardLeftPanel.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import NewsPanel from '../components/NewsPanel';
-import ErrorBoundary from '../components/ErrorBoundary';
-import { useData } from '../context/DataContext';
-import { useUI } from '../context/UIContext';
