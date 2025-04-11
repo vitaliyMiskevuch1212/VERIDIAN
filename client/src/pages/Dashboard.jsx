70d09d26b7c429e08ea0b79a6574048ea6748d48
vitaliyMiskevuch1212 <miskotop1234@gmail.com>
2025-04-11 18:10:54 +0300

    feat(hooks): scaffold useFinanceData hook with ticker quote fetch and loading state

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { DataProvider } from '../context/DataContext';
+import { UIProvider, useUI } from '../context/UIContext';
+import DashboardHeader from '../layouts/DashboardHeader';
+import DashboardLeftPanel from '../layouts/DashboardLeftPanel';
+import DashboardCenter from '../layouts/DashboardCenter';
+import DashboardRightPanel from '../layouts/DashboardRightPanel';
+import DashboardOverlays from '../layouts/DashboardOverlays';
+
