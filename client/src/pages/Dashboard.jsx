e93154bb81d071a67d3d427c336a57d7c28f511b
vitaliyMiskevuch1212 <miskotop1234@gmail.com>
2025-04-25 09:45:01 +0300

    Merge pull request #121 from Tiwari1782/feature/backend-frontend

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -7,3 +7,31 @@ import DashboardCenter from '../layouts/DashboardCenter';
 import DashboardRightPanel from '../layouts/DashboardRightPanel';
 import DashboardOverlays from '../layouts/DashboardOverlays';
 
+function CommandCenterLayout({ isInitialLoad, setIsInitialLoad }) {
+  const { defconLevel } = useUI();
+  return (
+    <div className="h-screen w-screen flex flex-col overflow-hidden" style={{ background: 'var(--defcon-bg-shift, var(--color-bg))', transition: 'background 1.5s ease' }}>
+      <DashboardHeader />
+      
+      {/* MAIN 3-COLUMN COMMAND CENTER */}
+      <div className="flex-1 relative overflow-hidden flex">
+        <DashboardLeftPanel />
+        <DashboardCenter />
+        <DashboardRightPanel />
+        <DashboardOverlays isInitialLoad={isInitialLoad} setIsInitialLoad={setIsInitialLoad} />
+      </div>
+    </div>
+  );
+}
+
+export default function Dashboard() {
+  const [isInitialLoad, setIsInitialLoad] = useState(true);
+
+  return (
+    <DataProvider>
+      <UIProvider>
+        <CommandCenterLayout isInitialLoad={isInitialLoad} setIsInitialLoad={setIsInitialLoad} />
+      </UIProvider>
+    </DataProvider>
+  );
+}
